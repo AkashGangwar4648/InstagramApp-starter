ec2c5cae82b8ceedb5fded2ff917bd4e3ac95a4c
Apple <apple@Apples-MacBook-Air.local>
2025-01-03 16:23:42 +0530

    Code done again-a

--- InstagramApp/ViewControllers/HomeScreenUI/HomeVC+CustomMethods.swift
+++ InstagramApp/ViewControllers/HomeScreenUI/HomeVC+CustomMethods.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import Foundation
-extension HomeVC {
+/*extension HomeVC {
     
     func initialSetup() {
         self.setupnavigationBar()
@@ -33,4 +33,30 @@ extension HomeVC {
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: rightBarItemImage, style: .plain, target: self, action: #selector(rightBarButtonTapped))
     }
     
+}*/
+extension HomeVC {
+ 
+    func initialSetup() {
+        self.tableView.register(UINib(nibName: "FeedTableViewCell", bundle: nil), forCellReuseIdentifier: "FeedTableViewCell")
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.setupNavigationBar()
+    }
+    
+    func setupNavigationBar() {
+        let profileImageView = UIImageView(image: UIImage(named: "logo_nav_icon"))
+        self.navigationItem.titleView = profileImageView
+        
+        var leftBarItemImage = UIImage(named: "camera_nav_icon")
+        leftBarItemImage = leftBarItemImage?.withRenderingMode(.alwaysOriginal)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: leftBarItemImage, style: .plain, target: self, action: #selector(leftBarButtonTapped))
+        
+        var rightBarItemImage = UIImage(named: "send_nav_icon")
+        rightBarItemImage = rightBarItemImage?.withRenderingMode(.alwaysOriginal)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: rightBarItemImage, style: .plain, target: self, action: #selector(rightBarButtonTapped))
+    }
+
 }
+
+
+
